5e182e1dde6a21d772561edbda1d0eac490b43d0
Hidayat <dayatznur@gmail.com>
2025-04-19 09:28:31 +0700

    fix bannner show from remote config

--- Assets/ColorLink/ColoroidCore/DM_Scripts/AdManager.cs
+++ Assets/ColorLink/ColoroidCore/DM_Scripts/AdManager.cs
@@ -9,19 +9,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Gley.MobileAds;
+using Firebase.RemoteConfig;
 
 
 public class AdManager : MonoBehaviour
 {
     private void Start()
     {
-        API.Initialize(OnInitialized);
+        StartCoroutine(WaitAndInitAds());
 
+    }
+
+    private IEnumerator WaitAndInitAds()
+    {
+        yield return new WaitForSeconds(1f);
+
+        API.Initialize(OnInitialized);
     }
 
     private void OnInitialized()
     {
-        API.ShowBanner(BannerPosition.Bottom, BannerType.Adaptive);
+        bool showBanner = FirebaseRemoteConfig.DefaultInstance.GetValue("showAdBanner").BooleanValue;
+        Debug.Log("showAdBanner from Remote Config: " + showBanner);
+
+        if (showBanner)
+        {
+            API.ShowBanner(BannerPosition.Bottom, BannerType.Adaptive);
+        }
 
         if (!API.GDPRConsentWasSet())
         {
--- Assets/FirebaseInit.cs
+++ Assets/FirebaseInit.cs
@@ -33,57 +33,35 @@ public class FirebaseInit : MonoBehaviour
     {
         // Set default values (optional)
         var defaults = new System.Collections.Generic.Dictionary<string, object>();
+        defaults.Add("showAdBanner", "false");
+        defaults.Add("showAdInter", "false");
+        defaults.Add("showAdNative", "false");
+        defaults.Add("showAdOpen", "false");
+        defaults.Add("showAdReward", "false");
 
-        #if UNITY_ANDROID
-        defaults.Add("banner", "ca-app-pub-3940256099942544/6300978111");
-        defaults.Add("inter", "ca-app-pub-3940256099942544/1033173712");
-        defaults.Add("native", "ca-app-pub-3940256099942544/2247696110");
-        defaults.Add("open", "ca-app-pub-3940256099942544/9257395921");
-        defaults.Add("reward", "ca-app-pub-3940256099942544/5224354917");
-
-        #elif UNITY_IOS
-        defaults.Add("banner", "ca-app-pub-3940256099942544/2934735716");
-        defaults.Add("inter", "ca-app-pub-3940256099942544/4411468910");
-        defaults.Add("native", "ca-app-pub-3940256099942544/3986624511");
-        defaults.Add("open", "ca-app-pub-3940256099942544/5662855259");
-        defaults.Add("reward", "ca-app-pub-3940256099942544/1712485313");
-
-        #else
-        defaults.Add("banner", "test_banner");
-        defaults.Add("inter", "test_interstitial");
-        defaults.Add("native", "test_native");
-        defaults.Add("open", "test_open");
-        defaults.Add("reward", "test_rewarded");
-        #endif
-
-        FirebaseRemoteConfig.DefaultInstance.SetDefaultsAsync(defaults);
-
-        // Fetch data
-        FirebaseRemoteConfig.DefaultInstance.FetchAsync(TimeSpan.Zero)
-            .ContinueWithOnMainThread(fetchTask =>
+        FirebaseRemoteConfig.DefaultInstance.SetDefaultsAsync(defaults).ContinueWithOnMainThread(defaultsTask => {
+            if (defaultsTask.IsCompleted)
             {
-                if (fetchTask.IsCompleted && !fetchTask.IsFaulted)
-                {
-                    Debug.Log("Remote Config fetched successfully!");
-                    FirebaseRemoteConfig.DefaultInstance.ActivateAsync()
-                        .ContinueWithOnMainThread(activateTask =>
+                // Baru fetch setelah set default selesai
+                FirebaseRemoteConfig.DefaultInstance.FetchAsync(TimeSpan.Zero)
+                    .ContinueWithOnMainThread(fetchTask =>
+                    {
+                        if (fetchTask.IsCompleted && !fetchTask.IsFaulted)
                         {
-                            // string welcomeMessage = FirebaseRemoteConfig.DefaultInstance.GetValue("welcome_message").StringValue;
-                            // Debug.Log("Remote Config value: welcome_message = " + welcomeMessage);
-                            // var keys = FirebaseRemoteConfig.DefaultInstance.Keys;
-
-                            // foreach (var key in keys)
-                            // {
-                            //     var value = FirebaseRemoteConfig.DefaultInstance.GetValue(key);
-                            //     Debug.Log($"[RemoteConfig] Key: {key}, Value: {value.StringValue}, Source: {value.Source}");
-                            // }
-                        });
-                }
-                else
-                {
-                    Debug.LogWarning("Remote Config fetch failed.");
-                }
-            });
+                            Debug.Log("Remote Config fetched successfully!");
+                            FirebaseRemoteConfig.DefaultInstance.ActivateAsync()
+                                .ContinueWithOnMainThread(activateTask =>
+                                {
+                                    Debug.Log("Remote Config values activated!");
+                                });
+                        }
+                        else
+                        {
+                            Debug.LogWarning("Remote Config fetch failed.");
+                        }
+                    });
+            }
+        });
     }
 
 }
