0e3bb5b6c0471288fc6532f698b6945e2e478279
Hidayat <dayatznur@gmail.com>
2025-04-29 10:13:52 +0700

    add condition android onesignal

--- Assets/ColorLink/DM_Scripts/NotificationManager.cs
+++ Assets/ColorLink/DM_Scripts/NotificationManager.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
 using OneSignalSDK;
+#if UNITY_ANDROID
 using Unity.Notifications.Android;
 using UnityEngine.Android;
+#endif
 
 public class NotificationManager : MonoBehaviour
 {
@@ -10,17 +12,20 @@ public class NotificationManager : MonoBehaviour
 		//Debug.Log("CHAY VAO DAY");
 		RequestAuthorization();
 
+    #if UNITY_ANDROID
 		// Replace 'YOUR_ONESIGNAL_APP_ID' with your OneSignal App ID from app.onesignal.com
 		OneSignal.Initialize("765a0604-86de-4c8b-a6e8-bf8ebe309028");
+		#endif
 	}
 
 	public void RequestAuthorization()
 	{
+		#if UNITY_ANDROID
 		Debug.Log("RequestUserPermission : " + Permission.HasUserAuthorizedPermission("android.permission.POST_NOTIFICATIONS"));
 		if (!Permission.HasUserAuthorizedPermission("android.permission.POST_NOTIFICATIONS"))
 		{
 			Permission.RequestUserPermission("android.permission.POST_NOTIFICATIONS");
 		}
-
+		#endif
 	}
 }
